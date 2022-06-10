38abae99b81a4de57f0f5d1606d105c1fb099a5a
Laramie Downs <laramiedowns1994@gmail.com>
2022-06-09 22:42:54 -0500

    Added get_sets function. Changed get_cards to redirect to price-guide page based on set name, then to output a Dict of the Card Names and Urls to a json doc.

--- selenium_method.py
+++ selenium_method.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
+import json
+
 driver = webdriver.Chrome("E:\Downloads\chromedriver.exe")
 driver.get("https://prices.tcgplayer.com/price-guide")
 driver.implicitly_wait(20)
@@ -11,34 +13,45 @@ driver.find_element(By.CLASS_NAME, "dfwid-close").click()
 
 
 Magic = {}
+set_list = []
 
-def get_card_info():
-    select = Select(driver.find_element(By.ID, "set"))
-    selected_option = select.first_selected_option
-    title = selected_option.text
-    print(title)
-    Magic[f'{title}'] = {}
+def get_sets():
+    select = Select(driver.find_element(By.ID, 'set'))
+    select_list = select.options
+    for option in select_list:
+        set_list.append(option.get_attribute('value'))
+
+
+def get_cards_in_set():
+    driver.get(f'https://prices.tcgplayer.com/price-guide/magic/{set_list[2]}')
+    # select = Select(driver.find_element(By.ID, "set"))
+    # selected_option = select.first_selected_option
+    # title = selected_option.text
+    # print(title)
+    Magic[f'{set_list[2]}'] = {}
 
     table = driver.find_element(By.CSS_SELECTOR, 'table')
     x = 0
     for row in table.find_elements(By.CSS_SELECTOR, 'tr'):
-        Magic[f'{title}'][f'{x}'] = {}
+        Magic[f'{set_list[2]}'][f'{x}'] = {}
         for cell in row.find_elements(By.CSS_SELECTOR, 'a'):
             # print(cell.text)
-            Magic[f'{title}'][f'{x}']['Card_Name'] = cell.text
+            Magic[f'{set_list[2]}'][f'{x}']['Card_Name'] = cell.text
         for cell in row.find_elements(By.CLASS_NAME, 'thumbnail'):
             href = cell.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
             # print(href)
-            Magic[f'{title}'][f'{x}']['Card_URL'] = href
-        for cell in row.find_elements(By.CLASS_NAME, "marketPrice"):
-            # print(cell.text)
-            Magic[f'{title}'][f'{x}']['Card_Market_Price'] = cell.text
-        print(Magic)
+            Magic[f'{set_list[2]}'][f'{x}']['Card_URL'] = href
         x += 1
+    
+    magic = json.dumps(Magic)
+    f = open("dict.json", "w")
+    f.write(magic)
+    f.close()
 
 
-get_card_info()
-print(Magic)
 
+
+get_sets()
+get_cards_in_set()
 
 driver.quit()
